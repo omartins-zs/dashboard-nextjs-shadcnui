fa753b844f05b90ac8a20b94847036295f6da723
omartins-zs <gabrielmatheus209@gmail.com>
2024-08-27 11:25:00 -0300

    Criando e Personalizando os Cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,77 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { BadgeDollarSign, DollarSign, Percent, Users } from "lucide-react";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Projeto Dashboard</h1>
-      <Button>Teste Button</Button>
+    <main className="sm:ml-14 p-4">
+      <section className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                Total vendas
+              </CardTitle>
+              <DollarSign className="ml-auto w-4 h-4" />
+            </div>
+            <CardDescription>Total de vendas em 90 dias</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">R$ 40.000</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                Novos clientes
+              </CardTitle>
+              <Users className="ml-auto w-4 h-4" />
+            </div>
+            <CardDescription>Novos clientes em 30 dias</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">234</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                Pedidos hoje
+              </CardTitle>
+              <Percent className="ml-auto w-4 h-4" />
+            </div>
+            <CardDescription>Total de pedidos hoje</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">65</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                Total Pedidos
+              </CardTitle>
+              <BadgeDollarSign className="ml-auto w-4 h-4" />
+            </div>
+            <CardDescription>Total de pedidos em 30 dias</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">2300</p>
+          </CardContent>
+        </Card>
+      </section>
     </main>
   );
 }
