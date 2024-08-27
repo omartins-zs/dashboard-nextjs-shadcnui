0483ad395525134453b617e253366f4d68fc4c62
omartins-zs <gabrielmatheus209@gmail.com>
2024-08-27 08:56:20 -0300

    Instalando Component Card, Avatar, Sheet(Sidebar)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Projeto Dashboard</h1>
+      <h1>Projeto Dashboard</h1>
+      <Button>Teste Button</Button>
     </main>
   );
 }
